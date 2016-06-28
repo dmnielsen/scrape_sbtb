4de6883e8869287b9696444f1fc3f881fc11da81
dmnielsen <dmnielsen@gmail.com>
2016-06-28 00:11:44 -0500

    added dict to convert months to int

--- booklinks.py
+++ booklinks.py
@@ -1,6 +1,12 @@
 import sys,os
+import calendar as cal
 from bs4 import BeautifulSoup
 import requests
+
+def define_month_abbr():
+    # define dictionary to convert month_abbr to integer with leading zero
+    global months = {v: str(k).zfill(2) for k,v in enumerate(cal.month_abbr)}
+    
 
 if __name__ == '__main__':
     
@@ -12,7 +18,7 @@ if __name__ == '__main__':
     
         r = requests.get(baseurl+str(i)+'/')
     
-        soup = BeautifulSoup(r.text)
+        soup = BeautifulSoup(r.text,lxml)
     
         for entry in soup.find_all('h1',{'class':'entry-title'}):
             link = entry.find('a').get('href')
