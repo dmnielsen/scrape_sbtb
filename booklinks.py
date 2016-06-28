d8a4745d2fa2b2489bdf6f8581bfc63c146a5aa4
dmnielsen <dmnielsen@gmail.com>
2016-06-28 01:02:29 -0500

    parse for links and dates

--- booklinks.py
+++ booklinks.py
@@ -2,11 +2,13 @@ import sys,os
 import calendar as cal
 import datetime as dt
 from bs4 import BeautifulSoup
+import re
 import requests
 
 def define_month_abbr():
-    # define dictionary to convert month_abbr to integer with leading zero
-    global months = {v: k for k,v in enumerate(cal.month_abbr)}
+    # global define dict to convert month_abbr to integer with leading zero
+    global months
+    months = {v: k for k,v in enumerate(cal.month_abbr)}
     
 def format_date(d):
     # convert Mon DD, YYYY to YYYY-MM-DD
@@ -18,18 +20,32 @@ def format_date(d):
 
 if __name__ == '__main__':
     
+    define_month_abbr()
+    
     linkfile = open('entrylinks.txt','w')
     
-    baseurl = 'http://smartbitchestrashybooks.com/review/book/page/'
+    #baseurl = 'http://smartbitchestrashybooks.com/review/book/page/'
+    baseurl = os.getcwd()+'/testfiles/review_index'  # for testing
     
-    for i in range(0,1): #60 total pages: 27 June 2016
     
-        r = requests.get(baseurl+str(i)+'/')
+    for i in range(0,2): #60 total pages: 27 June 2016
+
+        fn = baseurl+str(i)+'.html'  # for testing
+        fhand = open(fn)  # for testing
     
-        soup = BeautifulSoup(r.text,lxml)
+        #fn = baseurl+str(i)+'/'
+        #r = requests.get(fn)
     
-        for entry in soup.find_all('h1',{'class':'entry-title'}):
-            link = entry.find('a').get('href')
-            linkfile.write(link+'\n')
+        soup = BeautifulSoup(fhand,'lxml')
+        
+        fhand.close() # for testing
+    
+        for entry in soup.find_all('header',{'class':'entry-header book'}):
+            book = entry.find('h1',{'class':'entry-title'})
+            link = book.a.get('href')
+            
+            meta_info = entry.p.text
+            x = re.findall('· ([A-Za-z]+ [0-9,]+ [0-9]+) .+ ·',meta_info)
+            date = format_date(x[0])
             
     linkfile.close()
