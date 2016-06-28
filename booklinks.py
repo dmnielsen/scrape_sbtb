667c08dfdabad5b33cc0766e84c132ee074ab048
dmnielsen <dmnielsen@gmail.com>
2016-06-28 00:28:14 -0500

    added date formatter

--- booklinks.py
+++ booklinks.py
@@ -5,8 +5,15 @@ import requests
 
 def define_month_abbr():
     # define dictionary to convert month_abbr to integer with leading zero
-    global months = {v: str(k).zfill(2) for k,v in enumerate(cal.month_abbr)}
+    global months = {v: k for k,v in enumerate(cal.month_abbr)}
     
+def format_date(d):
+    # convert Mon DD, YYYY to YYYY-MM-DD
+    d = d.replace(',','').split()
+    d[0] = months[d[0]]
+    d = [int(x) for x in d]
+    d_format = dt.date(d[2],d[0],d[1])
+    return d_format 
 
 if __name__ == '__main__':
     
@@ -14,7 +21,7 @@ if __name__ == '__main__':
     
     baseurl = 'http://smartbitchestrashybooks.com/review/book/page/'
     
-    for i in range(0,61): #60 total pages: 27 June 2016
+    for i in range(0,1): #60 total pages: 27 June 2016
     
         r = requests.get(baseurl+str(i)+'/')
     
