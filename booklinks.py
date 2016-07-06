3fd087ad8e7da024a94123b541346987cbf33f94
dmnielsen <dmnielsen@gmail.com>
2016-07-06 01:24:38 -0500

    Add id,date,link to database

--- booklinks.py
+++ booklinks.py
@@ -28,8 +28,20 @@ if __name__ == '__main__':
     cur = conn.cursor()
     
     cur.execute('''CREATE TABLE IF NOT EXISTS Reviews (\
-    Id INTEGER UNIQUE, Date TEXT, Link TEXT, Reviewer TEXT,\
+    Id INTEGER UNIQUE, Review_date TEXT, Link TEXT, Reviewer TEXT,\
     Grade TEXT, Title TEXT, Author TEXT, Pub_year INTEGER, Genres TEXT);''')
+    
+    start = 0
+    cur.execute('SELECT max(id) FROM Reviews')
+    
+    try:
+        row = cur.fetchone()
+        if row[0] is not None:
+            start = row[0]
+            print(row)
+    except:
+        start = 0
+        row = None
     
     #baseurl = 'http://smartbitchestrashybooks.com/review/book/page/'
     baseurl = os.getcwd()+'/testfiles/review_index'  # for testing
@@ -48,6 +60,8 @@ if __name__ == '__main__':
         fhand.close() # for testing
     
         for entry in soup.find_all('header',{'class':'entry-header book'}):
+            start += 1
+            
             book = entry.find('h1',{'class':'entry-title'})
             link = book.a.get('href')
             
@@ -55,5 +69,9 @@ if __name__ == '__main__':
             x = re.findall('· ([A-Za-z]+ [0-9,]+ [0-9]+) .+ ·',meta_info)
             date = format_date(x[0])
             
+            cur.execute('INSERT INTO Reviews (id,Review_date,link)\
+            VALUES (?, ?, ?)',(start,date,link))
+            
     linkfile.close()
+    conn.commit()
     cur.close()
