6d2cbfaae4b47a07c7a6a2bd7b6af944831b90e7
dmnielsen <dmnielsen@gmail.com>
2016-07-09 14:18:17 -0500

    Commented out SQL while testing html parsing

--- scrape_reviews.py
+++ scrape_reviews.py
@@ -2,14 +2,15 @@ import sys,os
 from bs4 import BeautifulSoup
 import urllib.request
 import sqlite3 as sql
+import glob # for testing
 
 if __name__ == '__main__':
     
+    """ COMMENT OUT FOR TESTING 
     conn = sql.connect('sbtb.db',timeout=100)
     cur = conn.cursor()
     
     cur.execute('SELECT Link From Reviews WHERE Grade IS NULL;')
-    
     
     while True:
         
@@ -19,3 +20,9 @@ if __name__ == '__main__':
             break
     
         html = urllib.request.urlopen(link[0]).read()
+    """
+    
+    links = glob.glob('testfiles/book*')
+    
+    for link in links:
+        pass
