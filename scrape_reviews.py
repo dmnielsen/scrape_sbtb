a3ed2af9e1838b82b78619237a290c9eb16be4fb
dmnielsen <dmnielsen@gmail.com>
2016-07-09 14:04:52 -0500

    Open DB, loop thru entries with no grade

--- scrape_reviews.py
+++ scrape_reviews.py
@@ -5,4 +5,17 @@ import sqlite3 as sql
 
 if __name__ == '__main__':
     
-    pass
+    conn = sql.connect('sbtb.db',timeout=100)
+    cur = conn.cursor()
+    
+    cur.execute('SELECT Link From Reviews WHERE Grade IS NULL;')
+    
+    
+    while True:
+        
+        link = cur.fetchone()
+        
+        if link == None:
+            break
+    
+        html = urllib.request.urlopen(link[0]).read()
