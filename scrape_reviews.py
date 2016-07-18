fb29d790edab9b992f6cbfba6cdc38e5fb649179
dmnielsen <dmnielsen@gmail.com>
2016-07-18 16:34:34 -0500

    added lines to find and capture reviewer,grade,title/author,genre,pub_year

--- scrape_reviews.py
+++ scrape_reviews.py
@@ -29,3 +29,23 @@ if __name__ == '__main__':
         
         grade = review.find('h1',{'class':'grade'}).text
         reviewer = review.find('div',{'class':'entry-meta'}).a.text
+        
+        try:
+            genres = [genre.text for genre in review.find(
+            'div',{'class':'callout'}).find_all('a')[1:]]
+            pub_info = review.find('div',{'class':'featured'}).find(
+            'p',{'class':'pub'}).text.split()
+            pub_year = pub_info[pub_info.index('·')-1]
+        except AttributeError:
+            try:
+                genre = review.find(
+                'div',{'class':'review-box'}).text.strip().split('\n')
+                genres = [genre[-1].split(':')[1]]
+                print([s for s in genre if s.startswith('Publication')][0].split())
+            except IndexError:
+                print('genre problem',link)
+                continue
+            
+        titleauthor = review.find('h1',{'class':'entry-title'}).text
+        
+        print(grade,reviewer,genres,titleauthor,'\n')
