fe1c68894a5d03136444195cc97a0660c086a18e
dmnielsen <dmnielsen@gmail.com>
2016-07-18 16:45:49 -0500

    generalized the finding of publication years with the old format of site

--- scrape_reviews.py
+++ scrape_reviews.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import urllib.request
 import sqlite3 as sql
 import glob # for testing
+import re
 
 if __name__ == '__main__':
     
@@ -41,11 +42,21 @@ if __name__ == '__main__':
                 genre = review.find(
                 'div',{'class':'review-box'}).text.strip().split('\n')
                 genres = [genre[-1].split(':')[1]]
-                print([s for s in genre if s.startswith('Publication')][0].split())
+                pub_info = [s for s in genre if s.startswith('Publication')][0]
+                pub_years = (re.findall('\d{4}',pub_info))
+                if len(pub_years) < 1:
+                    pub_year = -9999
+                    print("no pub_year: ",link)
+                elif len(pub_years) > 1:
+                    print("multiple pub_years: ",pub_years,link)
+                    pub_year = pub_years[0]
+                else:
+                    pub_year = pub_years[0]
+                
             except IndexError:
                 print('genre problem',link)
                 continue
             
         titleauthor = review.find('h1',{'class':'entry-title'}).text
         
-        print(grade,reviewer,genres,titleauthor,'\n')
+        print(grade,reviewer,genres,titleauthor,pub_year,'\n')
