8c114a45d5ae3bb10f43e208040d8c3c791435ce
dmnielsen <dmnielsen@gmail.com>
2016-08-08 11:15:56 -0500

    split title/author

--- scrape_reviews.py
+++ scrape_reviews.py
@@ -7,13 +7,16 @@ import re
 
 if __name__ == '__main__':
     
-    """ COMMENT OUT FOR TESTING 
+    #COMMENT OUT FOR TESTING 
+    """
     conn = sql.connect('sbtb.db',timeout=100)
     cur = conn.cursor()
     
     cur.execute('SELECT Link From Reviews WHERE Grade IS NULL;')
     
-    while True:
+    test = 0
+    
+    while test < 2:
         
         link = cur.fetchone()
         
@@ -21,8 +24,53 @@ if __name__ == '__main__':
             break
     
         html = urllib.request.urlopen(link[0]).read()
-    """
+        review = BeautifulSoup(html,'lxml').article
+        
+        grade = review.find('h1',{'class':'grade'}).text
+        reviewer = review.find('div',{'class':'entry-meta'}).a.text
+        
+        try:
+            genres = [genre.text for genre in review.find(
+            'div',{'class':'callout'}).find_all('a')[1:]]
+            pub_info = review.find('div',{'class':'featured'}).find(
+            'p',{'class':'pub'}).text
+            pub_years = re.findall(r'\d{4}',pub_info)
+            if len(pub_years) < 1:
+                pub_year = -9999
+                print("no pub_year: ",link)
+            elif len(pub_years) > 1:
+                print("multiple pub_years: ",pub_years,link)
+                pub_year = pub_years[0]
+            else:
+                pub_year = pub_years[0]
+        except AttributeError:
+            try:
+                genre = review.find(
+                'div',{'class':'review-box'}).text.strip().split('\n')
+                genres = [genre[-1].split(':')[1]]
+                pub_info = [s for s in genre if s.startswith('Publication')][0]
+                pub_years = (re.findall('\d{4}',pub_info))
+                if len(pub_years) < 1:
+                    pub_year = -9999
+                    print("no pub_year: ",link)
+                elif len(pub_years) > 1:
+                    print("multiple pub_years: ",pub_years,link)
+                    pub_year = pub_years[0]
+                else:
+                    pub_year = pub_years[0]
+                
+            except IndexError:
+                print('genre problem',link)
+                continue
+            
+        titleauthor = review.find('h1',{'class':'entry-title'}).text
+        
+        
+        print(grade,reviewer,genres,titleauthor,pub_year,'\n')
+        
+        test += 1
     
+    """
     links = glob.glob('testfiles/book*')
     
     for link in links:
@@ -67,4 +115,14 @@ if __name__ == '__main__':
             
         titleauthor = review.find('h1',{'class':'entry-title'}).text
         
-        print(grade,reviewer,genres,titleauthor,pub_year,'\n')
+        ind = (titleauthor.rfind('by'))
+        if ind < 0:
+            title = titleauthor
+            author = ''
+            print('titleauthor issue',link)
+        else:
+            title = titleauthor[:ind]
+            author = titleauthor[ind+3:]
+        print(author)
+        
+        print(grade,reviewer,genres,title,author,pub_year,'\n')
