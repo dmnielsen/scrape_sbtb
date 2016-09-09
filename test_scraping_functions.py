adbadb7aacafbe9a0de8c31ef2759c3f42e0c124
dmnielsen <dmnielsen@gmail.com>
2016-09-09 15:20:39 -0500

    separate tests of locally stored html files from main code

---
+++ test_scraping_functions.py
@@ -0,0 +1,19 @@
+import glob
+import scraping_functions as scrape
+
+if __name__ == '__main__':
+    
+    links = glob.glob('testfiles/new_book*')
+    
+    for link in links:
+        print(link)
+        if 'new' in link:
+            date = '2016-09-09'
+        else:
+            date = '2013-01-01'
+        
+        grade,reviewer,title,author,genres,pub_year = \
+        scrape.scrape_info(link,date)
+        
+        
+        print(grade,reviewer,title,author,genres,pub_year,'\n')
