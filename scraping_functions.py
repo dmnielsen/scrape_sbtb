92cc9643478a2d0374a42f18fdd42f0cbbf1cad5
dmnielsen <dmnielsen@gmail.com>
2016-09-18 20:59:22 -0500

    add error message to print errors

--- scraping_functions.py
+++ scraping_functions.py
@@ -17,7 +17,11 @@ def scrape_info(link, date):
         info = scrape_new_format(review)
     else:
         info = scrape_old_format(review)
-    return info
+        
+    if info[-1]:
+        print(link,info[-1])
+    
+    return info[:-1]
 
 
 def parse_webpage(link):
@@ -38,21 +42,27 @@ def scrape_new_format(review):
     """Find and return scraped info from reviews with new format
     Returns: grade,reviewer,title,author,genres,pub_year
     """
-    grade = get_grade(review)
-    reviewer, title, author = get_new_reviewertitleauthor(review)
-    genres = get_new_genres(review)
-    pub_year = get_new_pubyear(review)
-    return grade, reviewer, title, author, genres, pub_year
+    grade, e1 = get_grade(review)
+    reviewer, title, author, e2 = get_new_reviewertitleauthor(review)
+    genres, e3 = get_new_genres(review)
+    pub_year, e4 = get_new_pubyear(review)
+    
+    error = ', '.join([e1,e2,e3,e4])
+    
+    return grade, reviewer, title, author, genres, pub_year, error
 
 
 def scrape_old_format(review):
     """Find and return scraped info from reviews with old format
     Returns: grade, reviewer, title, author, genres, pub_year"""
-    grade = get_grade(review)
-    reviewer, title, author = get_old_reviewertitleauthor(review)
-    genres = get_old_genres(review)
-    pub_year = get_old_pubyear(review)
-    return grade, reviewer, title, author, genres, pub_year
+    grade, e1 = get_grade(review)
+    reviewer, title, author, e2 = get_old_reviewertitleauthor(review)
+    genres, e3 = get_old_genres(review)
+    pub_year, e4 = get_old_pubyear(review)
+    
+    error = ', '.join([e1,e2,e3,e4])
+
+    return grade, reviewer, title, author, genres, pub_year, error
 
 
 def get_grade(html_text):
@@ -62,29 +72,38 @@ def get_grade(html_text):
     try:
         grade = html_text.find('h1', {'class': 'grade'}).text
     except:
-        print('grade problem')
-        grade = 'N/A'
-    return grade
+        #print('grade problem')
+        grade = 'N/A', 'grade problem'
+    return grade, ''
 
 
 def get_new_reviewertitleauthor(html_text):
-    """Returns reviewer,title,author for new format reviews"""
+    """Returns reviewer,title,author for new format reviews
+    If any errors occur, return descriptive string
+    If no errors occur, return empty string
+    """
     review_title = html_text.find('h1', {'class': 'entry-title'}).text
 
     if ('guest' and 'review') in review_title.lower():
         return get_new_guestreview(html_text)
 
-    reviewer = get_reviewer(html_text)
-    title, author = get_new_titleauthor(html_text)
+    reviewer, e1 = get_reviewer(html_text)
+    title, author, e2 = get_new_titleauthor(html_text)
 
     if title is None:
         title = review_title
-    return reviewer, title, author
+
+    error = ', '.join([e1,e2])
+
+    return reviewer, title, author, error
 
 
 def get_new_guestreview(html_text):
-    """Returns reviewer info, title,author for new format guest reviews"""
-    return '', '', ''
+    """Returns reviewer info, title,author for new format guest reviews
+    If any errors occur, return descriptive string
+    If no errors occur, return empty string
+    """
+    return '', '', '', ''
 
 
 def get_old_reviewertitleauthor(html_text):
@@ -94,14 +113,17 @@ def get_old_reviewertitleauthor(html_text):
     if ('guest' and 'review') in review_title.lower():
         return get_old_guestreview(html_text)
 
-    reviewer = get_reviewer(html_text)
-    title, author = get_old_titleauthor(html_text)
-    return reviewer, title, author
+    reviewer, e1 = get_reviewer(html_text)
+    title, author, e2 = get_old_titleauthor(html_text)
+
+    error = ', '.join([e1, e2])
+
+    return reviewer, title, author, error
 
 
 def get_old_guestreview(html_text):
     """Returns reviewer info, title,author for old format guest reviews"""
-    return '', '', ''
+    return '', '', '', ''
 
 
 def get_reviewer(html_text):
@@ -109,9 +131,9 @@ def get_reviewer(html_text):
     try:
         reviewer = html_text.find('div', {'class': 'entry-meta'}).a.text
     except:
-        print('reviewer issue', link)
-        reviewer = 'N/A'
-    return reviewer
+        #print('reviewer issue')
+        reviewer = 'N/A', 'reviewer issue'
+    return reviewer, ''
 
 
 def get_new_titleauthor(html_text):
@@ -119,24 +141,24 @@ def get_new_titleauthor(html_text):
     try:
         details = html_text.find('div', {'class': 'hide-for-mobile-large'})
     except:
-        print("problem with title author details")
-        return None, None
+        #print("problem with title author details")
+        return None, None, "problem with title author details"
     try:
         title = details.h6.em.text
     except AttributeError:
-        print("No title found")
-        return None, None
+        #print("No title found")
+        return None, None, "No title found"
     try:
         author = details.find('p', {'class': 'small'}).a.text
     except AttributeError:
-        print("No author found")
-        return title, None
-    return title, author
+        #print("No author found")
+        return title, None, "No author found"
+    return title, author, ''
 
 
 def get_old_titleauthor(html_text):
-    """Return title and author for new format reviews"""
-    return '', ''
+    """Return title and author for old format reviews"""
+    return '', '', ''
 
 
 def get_new_genres(html_text):
@@ -145,14 +167,14 @@ def get_new_genres(html_text):
         genres = [genre.text for genre in html_text.find(
             'div', {'class': 'callout'}).find_all('a')[1:]]
     except AttributeError:
-        print('issue with genres')
-        return ''
-    return ' '.join(genres)
+        #print('issue with genres')
+        return '', 'issue with genres'
+    return ' '.join(genres), ''
 
 
 def get_old_genres(html_text):
     """Return genres from new format reviews"""
-    return ''
+    return '', ''
 
 
 def get_new_pubyear(html_text):
@@ -162,7 +184,7 @@ def get_new_pubyear(html_text):
                    'p', {'class': 'pub'}).text
     except AttributeError:
         print('no publication info found')
-        return None
+        return None, 'no publication info found'
     pub_years = re.findall(r'\d{4}', pub_info)
     if len(pub_years) < 1:
         pub_year = None
@@ -172,12 +194,12 @@ def get_new_pubyear(html_text):
         pub_year = min(pub_years)
     else:
         pub_year = pub_years[0]
-    return pub_year
+    return pub_year, ''
 
 
 def get_old_pubyear(html_text):
     """Return publication year from new format reviews"""
-    return ''
+    return '', ''
 
 
 if __name__ == '__main__':
