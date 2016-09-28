6c5a0efd9d2933bbc5c753cf9e9caaa9d1dba97e
dmnielsen <dmnielsen@gmail.com>
2016-09-28 10:37:13 -0400

    open and close sql connection

--- clean_old_reviews.py
+++ clean_old_reviews.py
@@ -1,4 +1,9 @@
 import sys,os
 
 if __name__ == '__main__':
-    pass
+    cutoff_date = '2014-10-28'
+    
+    conn = sql.connect('sbtb.db', timeout=100)
+    cur = conn.cursor()
+    
+    conn.close()
